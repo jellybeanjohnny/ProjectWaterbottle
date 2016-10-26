1d15d7f5ebfa6e5325698c6575753b0ba0b12dcf
jellybeanjohnny <mamerige@icloud.com>
2016-10-25 20:38:54 -0700

    Adds Jellybean framework

--- WaterBottleFramework/CardModel.swift
+++ WaterBottleFramework/CardModel.swift
@@ -7,15 +7,30 @@
 //
 
 import Foundation
+import Jellybean
 
-public struct CardModel {
+public class CardModel : NSCoding {
   
- public var frontText: String
- public var backText: String
+  public var frontText: String
+  public var backText: String
+  public let spacing: JBSpacing
   
- public init(frontText: String, backText: String) {
+  public init(frontText: String, backText: String) {
     self.frontText = frontText
     self.backText = backText
+    self.spacing = JBSpacing()
+  }
+  
+  public func encode(with aCoder: NSCoder) {
+    aCoder.encode(frontText, forKey:"frontText")
+    aCoder.encode(backText, forKey:"backText")
+    aCoder.encode(spacing, forKey:"spacing")
+  }
+  
+  public required init?(coder aDecoder: NSCoder) {
+    self.frontText = aDecoder.decodeObject(forKey: "frontText") as! String
+    self.backText = aDecoder.decodeObject(forKey: "backText") as! String
+    self.spacing = aDecoder.decodeObject(forKey: "spacing") as! JBSpacing
   }
   
 }
