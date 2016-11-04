059601450df1b13637fac37e2c422a6f62610ce8
jellybeanjohnny <mamerige@gmail.com>
2016-11-03 20:07:46 -0700

    Rewrites logic for displaying a card to be more clear. Fixes issue where closing and reopening the app would cause the card to hide and show

--- ProjectWaterBottle/ViewController.swift
+++ ProjectWaterBottle/ViewController.swift
@@ -10,109 +10,123 @@ import UIKit
 import WaterBottleCard
 
 class ViewController: UIViewController {
-  
-  @IBOutlet weak var frontTextView: UITextView!
-  @IBOutlet weak var backTextView: UITextView!
-  @IBOutlet weak var textStackView: UIStackView!
-  
-  @IBOutlet weak var completedLabel: UILabel!
-  
-  @IBOutlet weak var answerButtonsStackView: UIStackView!
-  
-  
-  var currentCard: CardModel?
-  
-  
-  
-  var currentIndex = 0
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
     
-    let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(showBack))
-    textStackView.addGestureRecognizer(tapRecognizer)
+    @IBOutlet weak var frontTextView: UITextView!
+    @IBOutlet weak var backTextView: UITextView!
+    @IBOutlet weak var textStackView: UIStackView!
     
-    NotificationCenter.default.addObserver(self, selector: #selector(handleRefresh), name: .onCardStoreRefresh, object: nil)
+    @IBOutlet weak var completedLabel: UILabel!
     
-  }
-
-  func handleRefresh() {
-    if !CardDataStore.sharedStore.dueCards.isEmpty {
-      setupReviewSession()
-    } else {
-      displayFinishedMessage()
+    @IBOutlet weak var answerButtonsStackView: UIStackView!
+    
+    
+    var currentCard: CardModel?
+    
+    var currentIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(showBack))
+        textStackView.addGestureRecognizer(tapRecognizer)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleRefresh), name: .onCardStoreRefresh, object: nil)
+        
     }
-  }
-  
-  func setupReviewSession() {
     
-    backTextView.isHidden = true
-    frontTextView.isHidden = false
-    completedLabel.isHidden = true
-    textStackView.isHidden = false
-    textStackView.isUserInteractionEnabled = true
-    answerButtonsStackView.isHidden = false
-    nextCard()
-
-  }
-  
-  @IBAction func correctButtonTapped() {
-    currentCard?.markCorrect()
-    nextCard()
-  }
-  
-  @IBAction func incorrectButtonTapped() {
-    currentCard?.markIncorrect()
-    nextCard()
-  }
-  
-  func presentCard(forIndex index: Int) {
-    
-    if index < CardDataStore.sharedStore.dueCards.count && !CardDataStore.sharedStore.dueCards.isEmpty {
-      let card = CardDataStore.sharedStore.dueCards[index]
-      currentCard = card
-      frontTextView.attributedText = card.frontAttributedText
-      backTextView.text = card.backText
+    func handleRefresh() {
+        
+        print("Cards: \(CardDataStore.sharedStore.allCards.count)\nDue: \(CardDataStore.sharedStore.dueCards.count)")
+        
+        if !CardDataStore.sharedStore.dueCards.isEmpty {
+            setupReviewSession()
+        } else {
+            displayFinishedMessage()
+        }
     }
-  }
-  
-  
-  func nextCard() {
     
-    answerButtonsStackView.isHidden = true
-    CardDataStore.sharedStore.syncWithUserDefaults()
-    
-    if currentIndex < CardDataStore.sharedStore.dueCards.count {
-      presentCard(forIndex: currentIndex)
-      currentIndex += 1
-      backTextView.isHidden = true
-    } else {
-      // no more cards!
-      displayFinishedMessage()
+    func setupReviewSession() {
+        showCardFront()
+        hideCardBackAndAnswers()
+        displayCard()
     }
-  }
-  
-  
-  
-  func showBack() {
-    backTextView.isHidden = false
-    answerButtonsStackView.isHidden = false
-  }
-  
-  
-  func displayFinishedMessage() {
-    resetIndex()
-    textStackView.isHidden = true
-    textStackView.isUserInteractionEnabled = false
-    completedLabel.isHidden = false
-    answerButtonsStackView.isHidden = true
-  }
-  
-  func resetIndex() {
-    currentIndex = 0
-  }
-
-  
+    
+    func showCardFront() {
+        frontTextView.isHidden = false
+        textStackView.isHidden = false
+        textStackView.isUserInteractionEnabled = true
+    }
+    
+    func hideCardBackAndAnswers() {
+        backTextView.isHidden = true
+        completedLabel.isHidden = true
+        answerButtonsStackView.isHidden = true
+    }
+    
+    @IBAction func correctButtonTapped() {
+        processAnswer(isCorrect: true)
+        displayCard()
+    }
+    
+    @IBAction func incorrectButtonTapped() {
+        processAnswer(isCorrect: false)
+        displayCard()
+    }
+    
+    func processAnswer(isCorrect: Bool) {
+        if isCorrect {
+            currentCard?.markCorrect()
+        } else {
+            currentCard?.markIncorrect()
+        }
+        advanceIndex()
+    }
+    
+    
+    func presentCard(_ card: CardModel) {
+        frontTextView.attributedText = card.frontAttributedText
+        backTextView.text = card.backText
+    }
+    
+    func nextCard() -> CardModel? {
+        if currentIndex < CardDataStore.sharedStore.dueCards.count {
+            return CardDataStore.sharedStore.dueCards[currentIndex]
+        }
+        return nil
+    }
+    
+    
+    func showBack() {
+        backTextView.isHidden = false
+        answerButtonsStackView.isHidden = false
+    }
+    
+    
+    func displayFinishedMessage() {
+        resetIndex()
+        textStackView.isHidden = true
+        textStackView.isUserInteractionEnabled = false
+        completedLabel.isHidden = false
+        answerButtonsStackView.isHidden = true
+    }
+    
+    func displayCard() {
+        if let next = nextCard() {
+            currentCard = next
+            presentCard(next)
+        } else {
+            displayFinishedMessage()
+        }
+    }
+    
+    func resetIndex() {
+        currentIndex = 0
+    }
+    
+    func advanceIndex() {
+        currentIndex += 1
+    }
+    
+    
 }
 
