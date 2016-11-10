13aef18f54413eed0b72f7739855fc125b9b2a67
jellybeanjohnny <mamerige@gmail.com>
2016-11-10 13:58:37 -0800

    Adds a Realm friendly model class called Card that will replace the current CardModel class

--- SharedCode/Card.swift
+++ SharedCode/Card.swift
@@ -12,22 +12,55 @@ import Jellybean
 
 class Card: Object {
   
-  public dynamic var frontText: String? = nil
-//  public dynamic var frontAttributedText: NSAttributedString? = nil
-  public dynamic var backText: String? = nil
-  
-  let spacing = JBSpacing()
-  
+  //MARK: - Public Properties
+  public dynamic var frontText: String?
+  public dynamic var backText: String?
+  public var frontAttributedText: NSAttributedString? // Realm should ignore
   public dynamic var dueDate: Date {
     return spacing.dueDate
   }
   
+  //MARK: - Private Properties
+  private(set) var spacing = JBSpacing() // Realm should ignore
+  dynamic var frontAttributedTextData: Data?
+  dynamic var spacingData: Data?
+  
+
+  
+  //MARK: - Spacing
   public func markCorrect() {
     spacing.increaseSpacing()
   }
   
   public func markIncorrect() {
     spacing.decreaseSpacing()
+  }
+  
+  //MARK: - Archiving/Unarchiving Data
+ 
+  /// Archives the current values in the spacing and attributed text properties into Data objects
+  /// so that they can be stored on Realm
+  public func archiveSpacingAndTextData() {
+    NSKeyedArchiver.archivedData(withRootObject: spacing)
+    if let frontAttributedText = frontAttributedText {
+      NSKeyedArchiver.archivedData(withRootObject: frontAttributedText)
+    }
+  }
+  
+  /// Unarchives any data that may be in the spacing and text data properties 
+  /// and sets the corresponding properties with those values
+  public func unarchiveSpacingAndTextData() {
+    if let spacingData = spacingData {
+      spacing = NSKeyedUnarchiver.unarchiveObject(with: spacingData) as! JBSpacing
+    }
+    
+    if let frontAttributedTextData = frontAttributedTextData {
+      frontAttributedText = NSKeyedUnarchiver.unarchiveObject(with: frontAttributedTextData) as? NSAttributedString
+    }
+  }
+  
+  override static func ignoredProperties() -> [String] {
+    return ["frontAttributedText", "spacing"]
   }
   
 }
